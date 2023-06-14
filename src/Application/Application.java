6a3efdec9697a9dbcc00f6c1eb70f38424241186
raimess11 <rahma.sakti24@gmail.com>
2023-06-14 16:26:54 +0700

    alur GUI

---
+++ src/Application/Application.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Application;
+
+/**
+ *
+ * @author rahma
+ */
+public class Application {
+    //exekusi program
+    //  menginisiasi program
+    //  menjalankan program
+    
+    
+    //memulai penggunaan Frame.
+    
+    //  *as Customer
+    //  menu login  -> menu utama   -> browse item          -> item Detail (rent)   -> confimation  <- menu utama
+    //                              -> manage rented item   -> item Detail (return) -> confimation  <- manage rented item
+    //                              -> TopUp                <- menu utama
+    
+    //  *as Owner
+    //  menu login  -> menu utama   -> manage Item  -> Add Item             <- manage Item
+    //                                              -> remove item          <- manage Item
+    //                                              -> edit item            <- manage Item
+    //                              -> notification -> confirmation return  <- notification
+}
