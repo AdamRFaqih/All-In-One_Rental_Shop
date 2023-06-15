58b92cbf33768cf2467896ee7083fc402e38eec9
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-06-16 06:16:23 +0700

    fix database

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import Database.CustomerDAO;
+import Database.GameDAO;
+import Database.ItemDAO;
 import Item.Game;
 import Item.Item;
 import User.Customer;
@@ -9,14 +11,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Game gow = new Game(123,"Game", "ini game",30.0,true,"God of War","RPG","Sony","good");
-        Game gta = new Game(124,"Game", "ini game",30.0,true,"GTA","RPG","Rockstar","good");
-        List<Item> rented = new ArrayList<>();
-        rented.add(gow);
-        rented.add(gta);
-        Customer customer = new Customer(0,"adam","adam@gmail.com","123","customer","08129908482","sukabirus",rented,"Junior",0,0);
-        CustomerDAO customerDAO = new CustomerDAO();
-        customerDAO.createData(customer);
+        Game gow = new Game(0,"Game", "ini game",30.0,true,"God of War","RPG","Sony","good");
+        Game gta = new Game(0,"Game", "ini game",30.0,true,"GTA","RPG","Rockstar","good");
+        GameDAO gameDAO = new GameDAO();
+        gameDAO.createData(gow);
+//        gameDAO.createData(gta);
+//        List<Item> rented = new ArrayList<>();
+//        rented.add(gow);
+//        Customer customer = new Customer(0,"adam","adam@gmail.com","123","customer","08129908482","sukabirus",rented,"Junior",0,0);
+//        CustomerDAO customerDAO = new CustomerDAO();
+//        customerDAO.createData(customer);
+//        List<Customer> customers = customerDAO.readData();
+//        for (int i = 0; i < customers.size(); i++){
+//            System.out.println(customers.get(i));
+//        }
 
 
     }
