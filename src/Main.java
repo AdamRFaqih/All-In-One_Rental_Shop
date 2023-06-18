c890e29926f40bfe67fa43322f70153fb4f53889
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-06-18 20:02:47 +0700

    fix DAO

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import Database.CustomerDAO;
 import Database.GameDAO;
 import Database.ItemDAO;
-import Item.Game;
-import Item.Item;
+import Database.OwnerDAO;
+import Item.*;
 import User.Customer;
+import User.Owner;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -11,21 +12,30 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Game gow = new Game(0,"Game", "ini game",30.0,true,"God of War","RPG","Sony","good");
-        Game gta = new Game(0,"Game", "ini game",30.0,true,"GTA","RPG","Rockstar","good");
-        GameDAO gameDAO = new GameDAO();
-        gameDAO.createData(gow);
-//        gameDAO.createData(gta);
-//        List<Item> rented = new ArrayList<>();
-//        rented.add(gow);
-//        Customer customer = new Customer(0,"adam","adam@gmail.com","123","customer","08129908482","sukabirus",rented,"Junior",0,0);
+        OwnerDAO ownerDAO = new OwnerDAO();
+        List<Owner> owners = ownerDAO.readData();
+        Owner kamal = owners.stream().filter(idOwner -> idOwner.getUserID() == 41).findFirst().orElse(null);
+        List<Item> items = new ArrayList<>();
+        items.add(itemToAdd(16));
+        items.add(itemToAdd(17));
+        items.add(itemToAdd(18));
+        kamal.setItemRented(items);
+        ownerDAO.updateData(kamal);
+
 //        CustomerDAO customerDAO = new CustomerDAO();
-//        customerDAO.createData(customer);
 //        List<Customer> customers = customerDAO.readData();
 //        for (int i = 0; i < customers.size(); i++){
-//            System.out.println(customers.get(i));
+//            System.out.println(customers.get(i).getUserName());
+//            for (int j = 0; j < customers.get(i).getRentedItem().size(); i++){
+//                System.out.println(customers.get(i).getRentedItem().get(j).getName());
+//            }
 //        }
+    }
 
-
+    public static Item itemToAdd(int id) throws SQLException {
+        ItemDAO itemDAO = new ItemDAO();
+        List<Item> items = itemDAO.readData();
+        Item itemToAdd = items.stream().filter(idItem -> idItem.getItemID() == id).findFirst().orElse(null);
+        return itemToAdd;
     }
 }
