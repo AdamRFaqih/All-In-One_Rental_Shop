9161842257ec046286fc49ea8b31411919b4c714
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-06-18 23:43:32 +0700

    do testing

--- src/Controller/TopUpController.java
+++ src/Controller/TopUpController.java
@@ -8,12 +8,15 @@ import java.sql.SQLException;
 
 public class TopUpController {
     public static void addBalance(Customer customer, double balance) throws SQLException{
-        CustomerDAO customerDAO = new CustomerDAO();
-        double currentBalance = customer.getWallet();
-        double newBalance = currentBalance + balance;
-        customer.setWallet(newBalance);
-        
-        customerDAO.updateData(customer);
+//        CustomerDAO customerDAO = new CustomerDAO();
+//        double currentBalance = customer.getWallet();
+//        double newBalance = currentBalance + balance;
+//        customer.setWallet(newBalance);
+//
+//        customerDAO.updateData(customer);
+        for (int i = 0; i < customer.getRentedItem().size(); i++){
+            System.out.println(customer.getRentedItem().get(i));
+        }
         
     }
 }
