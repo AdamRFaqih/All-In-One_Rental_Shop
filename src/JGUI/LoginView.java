73b50e7ebc40f6cc93271d5b2200200d060d9f78
raimess11 <rahma.sakti24@gmail.com>
2023-06-19 12:17:34 +0700

    browse item and owner register

--- src/JGUI/LoginView.java
+++ src/JGUI/LoginView.java
@@ -157,7 +157,7 @@ public class LoginView extends NavigatableJFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         try{
-            LoginController.customerRegister(username.getText(), password.getText());
+            LoginController.ownerRegister(username.getText(), password.getText());
             message.setText("Register Success, you can login here");
             resetField();
         } catch (Exception e){
